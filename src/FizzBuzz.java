4310695aa8b1614abfc72c12ac59bc43e9f78694
potatOS <hanlinjue@gmail.com>
2021-10-13 15:28:03 +0800

    Create FizzBuzz.java

---
+++ src/FizzBuzz.java
@@ -0,0 +1,51 @@
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    private static FizzBuzz instance;
+
+    private FizzBuzz() {
+    }
+
+    public static FizzBuzz getInstance() {
+        if (instance != null) {
+            return instance;
+        }
+        return instance = new FizzBuzz();
+    }
+
+    public List<String> fizzBuzz(int n) {
+
+        List<String> resList = new LinkedList<>();
+        String fizz = "Fizz";
+        String Buzz = "Buzz";
+        String tmp = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    tmp = fizz + Buzz;
+                } else {
+                    if (i % 3 == 0) {
+                        tmp = fizz;
+                    }
+                    if (i % 5 == 0) {
+                        tmp = Buzz;
+                    }
+                }
+            } else {
+                tmp = String.valueOf(i);
+            }
+
+            resList.add(tmp);
+        }
+        return resList;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Class<?> clz = Class.forName("FizzBuzz");
+        System.out.println(
+                clz.getMethod("fizzBuzz", int.class).invoke(clz.getMethod("getInstance").invoke(new Object()), 5));
+    }
+}
