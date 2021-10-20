8653ebdf30e335610058106e5c25c58b607b1b75
potatOS <hanlinjue@gmail.com>
2021-10-20 11:58:04 +0800

    Create MinMoves.java

---
+++ src/MinMoves.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MinMoves {
+    public int minMoves(int[] nums) {
+        int n = nums.length - 1;
+        int ans = 0;
+        // 逆向
+        // int min = Arrays.stream(nums).min().getAsInt();
+        // for (int i = 0; i <= n; i++) {
+        //     ans += nums[i] - min;
+        // }
+
+        // 正向
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+           ans += (i + 1) * (nums[n - i] - nums[n - 1 - i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 5 };
+        System.out.println(new MinMoves().minMoves(nums));
+    }
+}
+
