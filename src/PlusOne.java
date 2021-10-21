cbd20217c2363827f5292919af6e68aa039c1d7b
potatOS <hanlinjue@gmail.com>
2021-10-21 09:55:34 +0800

    Create PlusOne.java

---
+++ src/PlusOne.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int tmp = 0;
+        int index = digits.length - 1;
+        boolean loopFlag = true;
+        digits[index] += 1;
+        while (loopFlag) {
+            digits[index] += tmp;
+            if (digits[index] >= 10) {
+                tmp = digits[index] / 10;
+                digits[index] %= 10;
+                if (index == 0) {
+                    int[] tmpArr = { tmp };
+                    digits = IntStream.concat(Arrays.stream(tmpArr), Arrays.stream(digits)).toArray();
+                    loopFlag = false;
+                }
+                index--;
+            } else {
+                loopFlag = false;
+            }
+        }
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = { 9, 9, 9 };
+        int[] ans = new PlusOne().plusOne(digits);
+        System.out.print("{ ");
+        for (int i = 0; i < ans.length; i++) {
+            if (i == ans.length - 1) {
+                System.out.print(ans[i]);
+            } else {
+                System.out.print(ans[i] + ", ");
+            }
+        }
+        System.out.print(" }");
+    }
+}
