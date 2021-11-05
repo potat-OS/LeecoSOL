be955900e4f4c02fb1ccb9523e215c3d958e7016
potatOS <hanlinjue@gmail.com>
2021-11-05 11:15:33 +0800

    Create DistributeCandies.java

---
+++ src/DistributeCandies.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class DistributeCandies {
+    public int distributeCandies(int[] candyType) {
+        return Arrays.stream(candyType).distinct().count() > (candyType.length / 2) ? candyType.length / 2
+                : (int) Arrays.stream(candyType).distinct().count();
+    }
+
+    public static void main(String[] args) {
+        int[] candyType = { 1, 2, 3, 4 };
+        System.out.println(new DistributeCandies().distributeCandies(candyType));
+    }
+}
