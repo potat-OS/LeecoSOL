1fae87e9e6c36203e6021869065919e0cc3c10fe
potatOS <hanlinjue@gmail.com>
2021-11-05 22:09:18 +0800

    Create longestSubsequence.java

---
+++ src/LongestSubsequence.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubsequence {
+
+    public int longestSubsequence(int[] arr, int difference) {
+        int ans = 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int val : arr) {
+            map.put(val, map.getOrDefault(val - difference, 0) + 1);
+            ans = Integer.max(ans, map.get(val));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] arr = { 1, 2, 3, 4 };
+        int difference = 1;
+        Class<?> clz = Class.forName("LongestSubsequence");
+        System.out.println(clz.getMethod("longestSubsequence", int[].class, int.class)
+                .invoke(clz.getConstructor().newInstance(), arr, difference));
+    }
+}
