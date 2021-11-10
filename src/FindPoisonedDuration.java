9832fd1a6cc9d55d6c34e9bf1fce61e62649f606
potatOS <hanlinjue@gmail.com>
2021-11-10 11:29:05 +0800

    Create FindPoisonedDuration.java

---
+++ src/FindPoisonedDuration.java
@@ -0,0 +1,13 @@
+public class FindPoisonedDuration {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+
+        int ans = 0;
+        int interval = 0;
+        for (int i = 1; i < timeSeries.length; i++) {
+            interval = timeSeries[i] - timeSeries[i - 1];
+            ans += interval > duration ? duration : interval;
+        }
+
+        return ans + duration;
+    }
+}
