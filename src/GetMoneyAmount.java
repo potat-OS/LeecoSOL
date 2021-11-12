ba842399a4877422d9953f1125003679a3f8a3e7
potatOS <hanlinjue@gmail.com>
2021-11-12 14:26:53 +0800

    Create GetMoneyAmount.java

---
+++ src/GetMoneyAmount.java
@@ -0,0 +1,21 @@
+public class GetMoneyAmount {
+    public int getMoneyAmount(int n) {
+        int minCost = 0;
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                minCost = 20100;
+                for (int k = i; k < j; k++) {
+                    minCost = Integer.min(minCost, k + Integer.max(dp[i][k - 1], dp[k + 1][j]));
+                }
+                dp[i][j] = minCost;
+            }
+        }
+        return dp[1][n];
+    }
+
+    public static void main(String[] args) {
+        int n = 200;
+        System.out.println(new GetMoneyAmount().getMoneyAmount(n));
+    }
+}
