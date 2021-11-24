bbd677d50598f7a04c873ecb653af507f906d48c
potatOS <hanlinjue@gmail.com>
2021-11-24 17:29:52 +0800

    Update OriginalDigits.java

--- OriginalDigits.java
+++ OriginalDigits.java
@@ -3,19 +3,19 @@ public class OriginalDigits {
         StringBuilder ans = new StringBuilder();
         int[] res = new int[10];
         int[] cup = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            cup[s.charAt(i) - 'a']++;
+        for (int i : s.toCharArray()) {
+            cup[i - 97]++;
         }
-        res[0] = cup['z' - 'a'];
-        res[2] = cup['w' - 'a'];
-        res[4] = cup['u' - 'a'];
-        res[6] = cup['x' - 'a'];
-        res[8] = cup['g' - 'a'];
-        res[3] = cup['h' - 'a'] - res[8];
-        res[5] = cup['f' - 'a'] - res[4];
-        res[7] = cup['v' - 'a'] - res[5];
-        res[9] = cup['i' - 'a'] - res[5] - res[6] - res[8];
-        res[1] = cup['o' - 'a'] - res[0] - res[2] - res[4];
+        res[0] = cup[25];
+        res[2] = cup[22];
+        res[4] = cup[20];
+        res[6] = cup[23];
+        res[8] = cup[6];
+        res[3] = cup[7] - res[8];
+        res[5] = cup[5] - res[4];
+        res[7] = cup[21] - res[5];
+        res[9] = cup[8] - res[5] - res[6] - res[8];
+        res[1] = cup[14] - res[0] - res[2] - res[4];
         for (int i = 0; i < res.length; i++) {
             for (int j = 0; j < res[i]; j++) {
                 ans.append(i);
