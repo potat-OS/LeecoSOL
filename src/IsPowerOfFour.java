ef5b428eca72d8e5bca066fa6e688362276f5eab
potatOS <hanlinjue@gmail.com>
2021-11-26 11:39:37 +0800

    Create IsPowerOfFour.java

---
+++ src/IsPowerOfFour.java
@@ -0,0 +1,26 @@
+// import java.util.HashSet;
+// import java.util.Set;
+
+public class IsPowerOfFour {
+    // Set<Long> set;
+
+    // public boolean isPowerOfFour(int n) {
+    //     set = new HashSet<>();
+    //     preAction();
+    //     return set.contains((long) n);
+    // }
+
+    // private void preAction() {
+    //     for (long i = 1; i <= Integer.MAX_VALUE; i *= 4) {
+    //         set.add(i);
+    //     }
+    // }
+
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0xAAAAAAAA) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IsPowerOfFour().isPowerOfFour(16));
+    }
+}
