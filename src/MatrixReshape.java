4437cf233eae880cff1e67ec6c4fdc9ce59f48f9
potatOS <hanlinjue@gmail.com>
2021-11-27 15:48:07 +0800

    Create MatrixReshape.java

---
+++ src/MatrixReshape.java
@@ -0,0 +1,14 @@
+class MatrixReshape {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;
+        }
+        int[][] ans = new int[r][c];
+        for (int i = 0; i < r * c; i++) {
+            ans[i / c][i % c] = mat[i / n][i % n];
+        }
+        return ans;
+    }
+}
