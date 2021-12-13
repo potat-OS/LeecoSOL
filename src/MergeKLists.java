a8d02e27fc3bb497e3abef2519e5d8fcab6fd5d7
potatOS <hanlinjue@gmail.com>
2021-12-13 17:27:10 +0800

    Create MergeKLists.java

---
+++ src/MergeKLists.java
@@ -0,0 +1,28 @@
+import util.ListNode;
+
+class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = new ListNode();
+        ListNode tmpAns = ans;
+        for (int i = 0; i < lists.length; i++) {
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode();
+        node1.add(1);
+        node1.add(4);
+        node1.add(5);
+        ListNode node2 = new ListNode();
+        node1.add(1);
+        node1.add(3);
+        node1.add(4);
+        ListNode node3 = new ListNode();
+        node1.add(2);
+        node1.add(6);
+        System.out.println(new MergeKLists().mergeKLists(new ListNode[] { node1, node2, node3 }));
+    }
+}
