9b3e06abc3b68deb2cf377c6f06c5e44ff6a8d4a
potatOS <hanlinjue@gmail.com>
2021-12-17 15:05:49 +0800

    Update VisiblePoints.java

--- src/VisiblePoints.java
+++ src/VisiblePoints.java
@@ -1,62 +1,66 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 class VisiblePoints {
 
     public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
-        final int half = angle / 2, pLen = points.size();
-        final int ROUND = 360 - angle;
-        final int pos1 = location.get(0), pos2 = location.get(1);
-        int ans = 0, in = 0, d = -half;
-        Map<Double, Integer> cnt = new HashMap<>();
-        for (int i = 0; i < pLen; i++) {
-            int x = points.get(i).get(0) - pos1;
-            int y = points.get(i).get(1) - pos2;
-            if (x == 0 && x == y) {
-                in++;
-            } else {
-                Double degree = Math.atan2(y, x);
-                cnt.put(degree, cnt.getOrDefault(degree, 0) + 1);
+        List<Double> cnt = new ArrayList<>();
+        int same = 0, posX = location.get(0), posY = location.get(1);
+        // 计算并记录弧度
+        for (int i = 0; i < points.size(); i++) {
+            // 以视线源为原点
+            int x = points.get(i).get(0) - posX, y = points.get(i).get(1) - posY;
+            // 记录在原点的点位
+            if (x == 0 && y == x) {
+                same++;
+                continue;
             }
+            // 已将视线源作为原点，所以直接用三角函数计算弧度表示的 y/x 的反正切
+            cnt.add(Math.atan2(y, x));
         }
-        while (d < ROUND) {
-            Double a1 = (d + half) * Math.PI / 180,
-                    a2 = (d - half) * Math.PI / 180;
-            if (cnt.containsKey(a1) || cnt.containsKey(a1 + 1)) {
-                in += cnt.get(a1);
-            } else if (cnt.containsKey(a2) || cnt.containsKey(a2 + 1)) {
-                in -= cnt.get(a2);
+        Collections.sort(cnt);
+        // 由于存在d{pi} + angle > 180°的情况，可以在原数组中将每个元素
+        // d{pi} + 360°添加到原数组的后面，这样即可防止反转的问题。
+        int cntSize = cnt.size();
+        for (int i = 0; i < cntSize; i++) {
+            cnt.add(cnt.get(i) + 2 * Math.PI);
+        }
+        // 滑动窗口
+        int rt = 0, max = 0;
+        Double toRad = angle * Math.PI / 180;
+        cntSize = cnt.size();
+        for (int i = 0; i < cntSize; i++) {
+            Double rad = cnt.get(i) + toRad;
+            while (rt < cntSize && cnt.get(rt) <= rad) {
+                rt++;
             }
-            ans = Math.max(ans, in);
-            d++;
+            max = Math.max(rt - i, max);
         }
-        return ans;
+        return max + same;
     }
 
     public static void main(String[] args) {
         List<Integer> tmp1 = new ArrayList<>();
-        tmp1.add(2);
-        tmp1.add(1);
+        tmp1.add(0);
+        tmp1.add(0);
         List<Integer> tmp2 = new ArrayList<>();
+        tmp2.add(0);
         tmp2.add(2);
-        tmp2.add(2);
-        List<Integer> tmp3 = new ArrayList<>();
-        tmp3.add(3);
-        tmp3.add(4);
-        List<Integer> tmp4 = new ArrayList<>();
-        tmp4.add(1);
-        tmp4.add(1);
+        // List<Integer> tmp3 = new ArrayList<>();
+        // tmp3.add(3);
+        // tmp3.add(3);
+        // List<Integer> tmp4 = new ArrayList<>();
+        // tmp4.add(4);
+        // tmp4.add(4);
         List<List<Integer>> points = new ArrayList<>();
         points.add(tmp1);
         points.add(tmp2);
-        points.add(tmp3);
-        points.add(tmp4);
+        // points.add(tmp3);
+        // points.add(tmp4);
         List<Integer> location = new ArrayList<>();
         location.add(1);
         location.add(1);
-
         System.out.println(new VisiblePoints().visiblePoints(points, 90, location));
     }
 }
