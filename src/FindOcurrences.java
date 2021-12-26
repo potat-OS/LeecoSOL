5ea3843d99a918e9f93a8e38f87ccc6b5e0da46a
potatOS <hanlinjue@gmail.com>
2021-12-26 11:02:42 +0800

    Create FindOcurrences.java

---
+++ src/FindOcurrences.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class FindOcurrences {
+    public String[] findOcurrences(String text, String first, String second) {
+        String[] words = text.split(" ");
+        List<String> list = new ArrayList<String>();
+        for (int i = 2; i < words.length; i++) {
+            if (words[i - 2].equals(first) && words[i - 1].equals(second)) {
+                list.add(words[i]);
+            }
+        }
+        int size = list.size();
+        String[] ret = new String[size];
+        for (int i = 0; i < size; i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new FindOcurrences().findOcurrences(
+                        "obo jvezipre obo jnvavldde jvezipre jvezipre jnvavldde jvezipre jvezipre jvezipre y jnvavldde jnvavldde obo jnvavldde jnvavldde obo jnvavldde jnvavldde jvezipre",
+                        "jnvavldde", "y"));
+    }
+}
